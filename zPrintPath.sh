3a735dfcc59a25122369b7e2f910edb5eb54bf9f
onurarican <49713272+onurarican@users.noreply.github.com>
2019-04-17 11:12:21 +0100

    Update zPrintPath.sh

--- zPrintPath.sh
+++ zPrintPath.sh
@@ -12,7 +12,7 @@ for pathindex in $(eval echo "{1..$1}"); do
   [[ $MaxHopsThisPath -gt $MaxHopsWithinAllPaths ]] && MaxHopsWithinAllPaths=$MaxHopsThisPath
 done
 
-for hopindex in $(eval echo "{1..$MaxHopsWithinAllLoops}"); do
+for hopindex in $(eval echo "{1..$MaxHopsWithinAllPaths}"); do
   for pathindex in $(eval echo "{1..$1}"); do
     This_Hops_IP=echo "${array[$pathindex]}" | awk -v hopindex="$hopindex" '$1==hopindex { print $0 }' | awk -F  "[()]" '{print $2}'
     echo $hopindex - $This_Hops_IP
