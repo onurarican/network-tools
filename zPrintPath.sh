dc6b215d7940ba38310cade38434b1367d094924
onurarican <49713272+onurarican@users.noreply.github.com>
2019-04-17 14:17:21 +0100

    Update zPrintPath.sh

--- zPrintPath.sh
+++ zPrintPath.sh
@@ -6,7 +6,7 @@
 MaxHopsWithinAllPaths=0
 
 for pathindex in $(eval echo "{1..$1}"); do
-  echo "Path $pathindex / $1"
+  echo -en "\rPath $pathindex / $1"
   array[$pathindex]=`sudo traceroute -Tq 1 $3 -p $4 -w $2`
   MaxHopsThisPath=`echo "${array[$pathindex]}" | awk '{print $1}' | tail -1`
   [[ $MaxHopsThisPath -gt $MaxHopsWithinAllPaths ]] && MaxHopsWithinAllPaths=$MaxHopsThisPath
@@ -15,8 +15,13 @@ done
 for hopindex in $(eval echo "{1..$MaxHopsWithinAllPaths}"); do
   for pathindex in $(eval echo "{1..$1}"); do
     This_Hops_IP=`echo "${array[$pathindex]}" | awk -v hopindex="$hopindex" '$1==hopindex { print $0 }' | awk -F  "[()]" '{print $2}'`
-    echo $hopindex - $This_Hops_IP
+    [[ -z "$This_Hops_IP" ]] && continue
+    This_Hops_Name=`echo "${array[$pathindex]}" | awk -v hopindex="$hopindex" '$1==hopindex { print $0 }' | sed 's/(.*$//' | awk '{print $2}'`
+    This_Hops_Latency=`echo "${array[$pathindex]}" | awk -v hopindex="$hopindex" '$1==hopindex { print $0 }' | sed 's/^.*)//' | awk '{print $1 " " $2}'`
+    RawOutput=`echo "$RawOutput";echo $hopindex $This_Hops_IP $This_Hops_Name $This_Hops_Latency`
   done
 done
+
+echo "$RawOutput"
 
 exit 0
